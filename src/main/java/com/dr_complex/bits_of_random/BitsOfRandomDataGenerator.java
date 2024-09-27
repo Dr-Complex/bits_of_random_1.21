e04a34ca534b34883898e968c4cd84f1ebc5a93d
Dr Complex <michael@hanson.org.za>
2024-09-27 11:40:05 +0200

    Initial commit

---
+++ src/main/java/com/dr_complex/bits_of_random/BitsOfRandomDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dr_complex.bits_of_random;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BitsOfRandomDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
