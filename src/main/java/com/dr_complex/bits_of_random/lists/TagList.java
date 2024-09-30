1e072400207b21fffcdf56b3401ae5f71b45e959
Dr Complex <michael@hanson.org.za>
2024-09-30 08:29:05 +0200

    Version 0.0.1
    Added:
     Latex Item
     Latex Block
     Ruby Item
     Ruby Block
     Raw Ruby Item
     Raw Ruby Block
    
    Changed:
     Cleaned up the code.

--- src/main/java/com/dr_complex/bits_of_random/lists/TagList.java
+++ src/main/java/com/dr_complex/bits_of_random/lists/TagList.java
@@ -1,11 +1,5 @@
 package com.dr_complex.bits_of_random.lists;
 
-import com.dr_complex.bits_of_random.BitsOfRandom;
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.TagKey;
-
 public class TagList {
     public static class Items {
     }
